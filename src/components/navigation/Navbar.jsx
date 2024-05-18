861061da0ed5dfbacc4dde8a0d2de328804a2d22
agunje123 <andrija.gunjevic1@gmail.com>
2024-05-18 20:51:27 +0200

    Add basic navbar component

---
+++ src/components/navigation/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+	return (
+		<nav className="nav">
+			<a href="/" className="site_name">
+				A&A
+			</a>
+			<ul>
+				<li>FAQ</li>
+				<li>DI? ŠTO? KAD?</li>
+			</ul>
+		</nav>
+	);
+}
