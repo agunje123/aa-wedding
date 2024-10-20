8ea7183d99f9f39452fff332b1ccc85353c12633
agunje123 <andrija.gunjevic1@gmail.com>
2024-10-20 19:40:29 +0200

    Add contact info in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,8 +4,14 @@ export default function Footer() {
 	return (
 		<footer className="contact">
 			<header className="heading">Potvrdite svoj dolazak!</header>
-			<span>andrija.gunjevic1@gmail.com</span>
-			<span>vickovicantonija@gmail.com</span>
+			<div className="contactInfo">
+				<span>andrija.gunjevic1@gmail.com</span>
+				<span>+385 97 645 8605</span>
+			</div>
+			<div className="contactInfo">
+				<span>vickovicantonija@gmail.com</span>
+				<span>+385 98 668 264</span>
+			</div>
 		</footer>
 	);
 }
