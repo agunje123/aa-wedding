f7fda32b1a0113a4523ed06e2715a2d50516d843
agunje123 <andrija.gunjevic1@gmail.com>
2024-10-21 19:31:04 +0200

    Add multiple objects for card

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,29 @@ import "./styles/About.css";
 
 import InfoCard from "../components/info-card/InfoCard";
 
+const people = [
+	{
+		image: "src/assets/aa.png",
+		name: "Andrija i Antonija",
+		description: "Show",
+	},
+	{
+		image: "src/assets/tomo.png",
+		name: "Tomislav",
+		description: "HŽV",
+	},
+	{
+		image: "src/assets/mihi.png",
+		name: "Mihaela",
+		description: "Garo lover",
+	},
+	{
+		image: "src/assets/garo.png",
+		name: "Garo",
+		description: "Meow",
+	},
+];
+
 export default function About() {
 	return (
 		<>
@@ -9,11 +32,14 @@ export default function About() {
 				About
 			</h1>
 			<div className="cards">
-				<InfoCard
-					image="src/assets/disco-ball.png"
-					name="John Doe"
-					description="A short description about John Doe."
-				/>
+				{people.map((person, index) => (
+					<InfoCard
+						key={index}
+						image={person.image}
+						name={person.name}
+						description={person.description}
+					/>
+				))}
 			</div>
 		</>
 	);
