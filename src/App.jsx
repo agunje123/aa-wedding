4195b92048b1488160a0dc33752d70855dfeee12
agunje123 <andrija.gunjevic1@gmail.com>
2024-11-16 23:20:08 +0100

    Add invitation section, adjust margins

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./sections/Home";
+import Invitation from "./sections/Invitation";
 import About from "./sections/About";
 import Location from "./sections/Location";
 import Footer from "./sections/Footer";
@@ -13,7 +14,12 @@ function App() {
 		document.body.style.color = "var(--beige)";
 	}, []);
 
-	const sectionsRefs = [useRef(null), useRef(null), useRef(null)];
+	const sectionsRefs = [
+		useRef(null),
+		useRef(null),
+		useRef(null),
+		useRef(null),
+	];
 
 	useIntersectionObserver(sectionsRefs, { threshold: 0.25 });
 
@@ -22,12 +28,15 @@ function App() {
 			<Navbar />
 			<Home />
 			<div ref={sectionsRefs[0]} className="section">
-				<About />
+				<Invitation />
 			</div>
 			<div ref={sectionsRefs[1]} className="section">
-				<Location />
+				<About />
 			</div>
 			<div ref={sectionsRefs[2]} className="section">
+				<Location />
+			</div>
+			<div ref={sectionsRefs[3]} className="section">
 				<Footer />
 			</div>
 		</>
---
+++ src/sections/Invitation.jsx
@@ -0,0 +1,12 @@
+import "./styles/Invitation.css";
+
+export default function Invitation() {
+	return (
+		<>
+			<div className="message">
+				Napokon imamo odgovor na vječno pitanje "Dobro kad ćete se više
+				ženiti?"
+			</div>
+		</>
+	);
+}
