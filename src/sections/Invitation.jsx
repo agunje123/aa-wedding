ba1c0dedb0eb455b4e103ae3833b99f2b73769db
agunje123 <andrija.gunjevic1@gmail.com>
2025-02-04 11:09:33 +0100

    Embed video

--- src/sections/Invitation.jsx
+++ src/sections/Invitation.jsx
@@ -4,8 +4,15 @@ export default function Invitation() {
 	return (
 		<>
 			<div className="message">
-				Napokon imamo odgovor na vječno pitanje "Dobro kad ćete se više
-				ženiti?"
+				<iframe
+					width="315"
+					height="560"
+					src="https://www.youtube.com/embed/TU5nqumVRBU"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowfullscreen
+				></iframe>
 			</div>
 		</>
 	);
