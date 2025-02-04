9608344f332a7224d89a69053dbe3d9198a043e9
agunje123 <andrija.gunjevic1@gmail.com>
2025-02-04 13:28:37 +0100

    Add invitation response date

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import "./styles/Footer.css";
 export default function Footer() {
 	return (
 		<footer className="contact">
-			<header className="heading">Potvrdite svoj dolazak!</header>
+			<header className="heading">Potvrdite dolazak do 1. 4.!</header>
 			<div className="contactInfo">
 				<span>andrija.gunjevic1@gmail.com</span>
 				<span>+385 97 645 8605</span>
