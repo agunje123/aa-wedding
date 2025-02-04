615d7e5177385c3c52ca4fa897ec34bb817aa18c
agunje123 <andrija.gunjevic1@gmail.com>
2025-02-04 11:27:28 +0100

    Slight visual changes

--- src/sections/Invitation.jsx
+++ src/sections/Invitation.jsx
@@ -9,9 +9,9 @@ export default function Invitation() {
 					height="560"
 					src="https://www.youtube.com/embed/TU5nqumVRBU"
 					title="YouTube video player"
-					frameborder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-					allowfullscreen
+					allowFullScreen
+					frameborder="0"
 				></iframe>
 			</div>
 		</>
